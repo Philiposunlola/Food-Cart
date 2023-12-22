f70d995076c0e42269ecf7e69fb6da5d49e54d71
Philip <philiposun@gmail.com>
2023-12-22 01:50:14 +0100

    Update app.js

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ let quantity = document.querySelector('.quantity');
 openShopping.addEventListener('click', ()=>{
     body.classList.add('active');
 })
+
 closeShopping.addEventListener('click', ()=>{
     body.classList.remove('active');
 })
